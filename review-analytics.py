7826298a4c6050b5ba72926fff46d1f3270ef55b
Chiang <tien23@gmail.com>
2022-03-07 09:38:01 +0800

    update calc avg

--- review-analytics.py
+++ review-analytics.py
@@ -4,7 +4,13 @@ with open('reviews.txt', 'r') as f:
     for line in f :
         data.append(line)
         count += 1
-        if count % 10000 == 0 :
+        if count % 100000 == 0 :
             print(len(data))
-print(len(data))
-print(data[0])
+print('總共',len(data),'資料')
+
+sum_len =0 
+for line in data:
+    length = len(line)
+    sum_len += length
+print('留言平均長度',sum_len / len(data))
+
